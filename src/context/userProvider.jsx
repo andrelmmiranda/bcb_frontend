6a73ab0188067190f693ce6f03ca0e8ee7a89d4b
André Miranda <andrelmmiranda@gmail.com>
2022-03-23 12:44:48 -0300

    tentando fazer com que localstorage funcione corretamente.

--- src/context/userProvider.jsx
+++ src/context/userProvider.jsx
@@ -3,17 +3,13 @@ import { createContext, useState, useEffect } from 'react';
 export const UserContext = createContext({});
 
 export const UserProvider = ({ children })=>{
-    const userData = JSON.parse(localStorage.getItem('user'));
+    // const userData = JSON.parse(localStorage.getItem('user'));
+
+    const userData = (typeof JSON.parse(localStorage.getItem('user')) ? JSON.parse(localStorage.getItem('user')) : JSON.stringify({}))
 
     const [user, setUser] = useState(userData);
 
-    if(JSON.parse(localStorage.get('user')) === null){
-        localStorage.setItem('user', JSON.stringify({}));
-    }
-
-    let hasProperty = !user.hasOwnProperty('name') || user == null ? false : true;
-
-    const [isLogged, setIsLogged] = useState(hasProperty);
+    const [isLogged, setIsLogged] = useState(!user.hasOwnProperty('name') || user == null ? false : true);
 
     const setUserAtStorage = (user)=>{
         // Transforma o objeto em string e salva na localStorage
